e5913d3843c554d804dd0c1b1976467d73dbc5ad
Lesego <sego.mogane@gmail.com>
2023-04-20 11:30:49 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include<studio.h>
 /**
  * main - A program that prints a line with puts function
- *
  * Return: 0 (success)
  */
 int main(void)
@@ -9,6 +8,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
-
-
